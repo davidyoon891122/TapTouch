513313b85a24f30a6833b463c5da19517366dcc5
davidyoon <qqpo12@naver.com>
2021-07-07 23:17:34 +0900

    add tap and touch event func

--- TapTouch/ViewController.swift
+++ TapTouch/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtMessage: UILabel!
+    
+    @IBOutlet var txtTapCount: UILabel!
+    
+    @IBOutlet var txtTouchCount: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // 터치가 시작될 떄 호출
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch // 터치 이벤트 가져옴
+        
+        txtMessage.text = "Touches Began"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
+    
+    // 손가락을 움직였을 때
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Moved"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+        
+    }
+    // 손가락을 떼었을 때
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Ended"
+        txtTapCount.text = String(touch.tapCount)
+        txtTouchCount.text = String(touches.count)
+    }
 
 }
 
